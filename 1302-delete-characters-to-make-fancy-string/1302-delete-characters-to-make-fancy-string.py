f6abbe9e099ab54e61822b38d75e3e59250eeb0c
Angel  Sarpong Antwi-Mensah <antwiangel77@gmail.com>
2025-07-21 21:43:08 -0500

    Time: 343 ms (50.2%), Space: 19.2 MB (90.71%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,24 +1,24 @@
 """
 UnderStand - given a string, and we must remove characters so that no character appears three times in a row.
 Match - Sring manipulation;
-Plan - Initialize an empty list res = []
+Plan - Initialize an empty string res
 for char in s
 if  len(res) is greater than/ equal to 2 and res[-1] == res[-2] == char:
     move on to next line without doing nothing (skip it)
-Otherwise, append it to res.
-return joint list
+Otherwise, concatenate to res
+return res
 
 Implement 
 """
 
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        res = []
+        res = ''
         for char in s:
             if len(res) >= 2 and res[-1] == res[-2] == char:
-                continue #doesn't add 
-            res.append(char)
-        return ''.join(res)
+                continue #doesn't add (skips)
+            res+=char
+        return  res
 
 
 
@@ -27,6 +27,6 @@ class Solution:
 """
 Review -
 Evaluate - # Time Complexity is an O(n); where n is the length of a string
-#Space Complexity - is an O(n); where  n is the length of the array
+#Space Complexity - Optimized space complexity of O(1)
 """
         
